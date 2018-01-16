d61ba065fd26afb80148d29290a70fcf32cf367e
BL <bhushankumar.lilapara@gmail.com>
2018-01-16 19:51:08 +0530

    move to latest package fs-extra

--- examples/javaScript/feeds/submitFeed.js
+++ examples/javaScript/feeds/submitFeed.js
@@ -4,11 +4,12 @@ var accessKey = process.env.AWS_ACCESS_KEY_ID || 'YOUR_KEY';
 var accessSecret = process.env.AWS_SECRET_ACCESS_KEY || 'YOUR_SECRET';
 
 var amazonMws = require('../../../lib/amazon-mws')(accessKey, accessSecret);
-var fs = require('fs');
+var fse = require('fs-extra');
 
 var feedRequest = function () {
 
-    var FeedContent = fs.readFileSync('./file.txt', 'UTF-8');
+    var FeedContent = fse.readFileSync('./file.txt', 'UTF-8');
+    console.log('FeedContent ', FeedContent);
 
     amazonMws.feeds.submit({
         'Version': '2009-01-01',
