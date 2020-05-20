07294065a99bf5a81ac54b79096933ea92756d3d
BL <bhushankumar.lilapara@gmail.com>
2020-05-20 15:30:17 +0530

    Move array response to the Result object

--- examples/javaScript/products/getMatchingProductForId.js
+++ examples/javaScript/products/getMatchingProductForId.js
@@ -5,22 +5,23 @@ var accessSecret = process.env.AWS_SECRET_ACCESS_KEY || 'YOUR_SECRET';
 
 var amazonMws = require('../../../lib/amazon-mws')(accessKey, accessSecret);
 
-var productRequest = function () {
-    amazonMws.products.search({
+var productRequest = async function () {
+    var mwsRequestData = {
         Version: '2011-10-01',
         Action: 'GetMatchingProductForId',
-        SellerId: 'SELLER_ID',
-        MWSAuthToken: 'MWS_AUTH_TOKEN',
-        MarketplaceId: 'MARKET_PLACE_ID',
-        IdType: 'SellerSKU',
-        'IdList.Id.1': 'SKU'
-    }, function (error, response) {
-        if (error) {
-            console.log('error products', error);
-            return;
-        }
+        SellerId: 'AEDO7KP6Y1XO',
+        // MWSAuthToken: 'MWS_AUTH_TOKEN',
+        MarketplaceId: 'ATVPDKIKX0DER',
+        IdType: 'ASIN',
+        'IdList.Id.1': 'B00S59NYT2',
+        'IdList.Id.2': 'B01H5RLTLY'
+    };
+    try {
+        var response = await amazonMws.products.search(mwsRequestData);
         console.log('response', response);
-    });
+    } catch (error) {
+        console.log('error products', error);
+    }
 };
 
 productRequest();
